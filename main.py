9976a7bc7332cd7c74d9aaddfde25506965e7d47
Tega Rorobi <rorobitega@gmail.com>
2023-11-08 12:52:50 +0100

    Created endpoints for getting multiple books and creating them, (made use of just a temporary BOOKS array)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from uuid import UUID
 
@@ -12,3 +12,28 @@ class Book(BaseModel):
 
     def __str__(self):
         return self.title 
+
+
+app = FastAPI()
+
+BOOKS = []
+
+@app.get('/books')
+def get_books(count:int=None):
+    "Retrieve multiple books"
+    return BOOKS if count is None else BOOKS[:count]
+
+
+@app.post('/books')
+def create_book(book: Book):
+    "Create and return a new book"
+    BOOKS.append(book)
+    return book 
+
+
+
+
+if __name__ == '__main__':
+    import uvicorn 
+    filename:str = __file__.split('\\')[-1].split('.')[0]
+    uvicorn.run(f'{filename}:app', reload=True)
